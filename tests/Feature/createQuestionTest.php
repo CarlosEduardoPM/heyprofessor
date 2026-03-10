6106f76b4ed7b657cb030213cd076749a3b55ae8
CarlosEduardoPM <linkedincarlos321@gmail.com>
2026-03-10 16:01:27 -0300

    it should be able to create a new question bigger then 255 char

---
+++ tests/Feature/createQuestionTest.php
@@ -0,0 +1,17 @@
+<?php
+use App\Models\User;
+
+use function Pest\Laravel\{actingAs, assertDatabaseCount, post};
+
+it('should be able to create a new question bigger then 255 char', function () {
+    $user = User::factory()->create();
+    actingAs($user);
+
+    $request = post(route('question.store'), [
+        'question' => str_repeat('*', 260) . '?',
+
+    ]);
+    $request->assertRedirect(route('dashboard'));
+    assertDatabaseCount('questions', 1);
+    \Pest\Laravel\assertDatabaseHas('questions', ['question' => str_repeat('*', 260) . '?']);
+});

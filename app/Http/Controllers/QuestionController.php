28cd2148b4b6016589faa1f211c03529dc087691
CarlosEduardoPM <linkedincarlos321@gmail.com>
2026-03-11 15:40:35 -0300

    new rule for check ends with question mark

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
+use App\Rules\EndWithQuestionMarkRule;
 use Illuminate\Http\RedirectResponse;
 
 class QuestionController extends Controller
@@ -11,15 +12,12 @@ class QuestionController extends Controller
     {
 
         $attributes = request()->validate([
-            'question' => ['required', 'min:10', function (string $attribute, mixed $value, \Closure $fail) {
-
-                if ($value[strlen($value) - 1] !== '?') {
-                    $fail('Are you sure that is a question? it is missing the question mark in the end.');
-
-                }
-
-            }],
-
+            'question' => [
+                'required',
+                'min:10',
+                new EndWithQuestionMarkRule()
+                ,
+            ],
         ]);
 
         Question::query()->create(

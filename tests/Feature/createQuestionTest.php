899dec3445588defe656188530872ee92cb1df87
CarlosEduardoPM <linkedincarlos321@gmail.com>
2026-03-11 14:43:30 -0300

    it should have at least 10 char

--- tests/Feature/createQuestionTest.php
+++ tests/Feature/createQuestionTest.php
@@ -15,3 +15,16 @@ it('should be able to create a new question bigger then 255 char', function () {
     assertDatabaseCount('questions', 1);
     \Pest\Laravel\assertDatabaseHas('questions', ['question' => str_repeat('*', 260) . '?']);
 });
+
+it('should have at least 10 char', function () {
+    $user = User::factory()->create();
+    actingAs($user);
+
+    $request = post(route('question.store'), [
+        'question' => str_repeat('*', 8) . '?',
+
+    ]);
+
+    $request -> assertSessionHasErrors(['question' => __('validation.min.string', ['min' => 10, 'attribute' => 'question'])]);
+    assertDatabaseCount('questions', 0);
+});

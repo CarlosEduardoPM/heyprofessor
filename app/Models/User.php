afc012bcfc7740fd89198521c5dcb3aedd857f76
CarlosEduardoPM <linkedincarlos321@gmail.com>
2026-03-16 14:28:58 -0300

    feat: adding global function file

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,12 +65,14 @@ class User extends Authenticatable implements MustVerifyEmail
     public function like(Question $question): void
     {
 
-        $this->votes()->create([
-            'question_id' => $question->id,
-            'user_id'     => $this->id,
-            'like'        => 1,
-            'unlike'      => 0,
-        ]);
+        $this->votes()->updateOrCreate(
+            [
+                'question_id' => $question->id],
+            [
+                'like'   => 1,
+                'unlike' => 0,
+            ]
+        );
 
     }
 
---
+++ app/Support/Functions.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Models\User;
+
+function user(): ?User
+{
+    if (auth() ->check()) {
+
+        return auth()->user();
+
+    }
+
+    return null;
+
+}

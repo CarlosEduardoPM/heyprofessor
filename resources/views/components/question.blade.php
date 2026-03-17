f33f5479ad3aee0702b1c24bee905269e4cf106d
CarlosEduardoPM <linkedincarlos321@gmail.com>
2026-03-17 11:54:47 -0300

    feat: add dislike buttont

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -8,7 +8,7 @@
 items-center justify-between">
     <span>{{ $question->question  }}</span>
     <div class="flex gap-3 mt-2 ml-6 shrink-0">
-        <div class="p-2 bg-green-100 rounded-lg hover:bg-green-200 cursor-pointer">
+        <div class="p-2 bg-green-100 rounded-lg hover:bg-green-200 cursor-pointer w-12 flex justify-center">
 
 
             <x-post :action="route('question.like', $question)"><button class="flex items-center space-x-1 text-green-500">
@@ -18,11 +18,13 @@ items-center justify-between">
             </x-post>
         </div>
 
-        <div class="p-2 bg-red-100 rounded-lg hover:bg-red-200 cursor-pointer">
-            <a href="{{ route('question.like', $question) }}" class="flex items-center space-x-1 text-red-500">
+        <div class="p-2 bg-red-100 rounded-lg hover:bg-red-200 cursor-pointer w-12 flex justify-center">
+            <x-post :action="route('question.unlike', $question)"><button class="flex items-center space-x-1 text-red-500">
+
                 <x-icons.thumbs-down class="w-4 h-4 text-red-600"/>
                 <span> {{ $question->unlikes }} </span>
-            </a>
+            </button>
+            </x-post>
         </div>
     </div>
 

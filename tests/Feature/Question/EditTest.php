3d6fec34e3f43e48c72088602a9e58e06e47607f
CarlosEduardoPM <linkedincarlos321@gmail.com>
2026-03-30 12:37:39 -0300

    it should return a view

--- tests/Feature/Question/EditTest.php
+++ tests/Feature/Question/EditTest.php
@@ -2,7 +2,7 @@
 
 use App\Models\{Question, User};
 
-use function Pest\Laravel\actingAs;
+use function Pest\Laravel\{actingAs, get};
 
 it('should be able to open a question to edit', function () {
 
@@ -11,6 +11,17 @@ it('should be able to open a question to edit', function () {
 
     actingAs($user);
 
-    \Pest\Laravel\get(route('question.edit', $question))->assertSuccessful();
+    get(route('question.edit', $question))->assertSuccessful();
+
+});
+
+it('should return a view', function () {
+
+    $user     = User::factory()->create();
+    $question = Question::factory()->for($user, 'createdBy')->create();
+
+    actingAs($user);
+
+    get(route('question.edit', $question))->assertViewIs('question.edit');
 
 });
